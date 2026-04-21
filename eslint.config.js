2a10c621dc662542340aadb575edfe4fb1995301
Jamshed Rabadi <jamshedrabadi@gmail.com>
2026-04-21 03:44:05 -0700

    added editorconfig file for linting, added lots of linting rules, changed indentation to tabs size 4

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,36 @@
 // https://docs.expo.dev/guides/using-eslint/
-const { defineConfig } = require('eslint/config');
-const expoConfig = require('eslint-config-expo/flat');
+import stylistic from '@stylistic/eslint-plugin';
+import expoConfig from 'eslint-config-expo/flat.js';
+import { defineConfig } from 'eslint/config';
 
-module.exports = defineConfig([
-  expoConfig,
-  {
-    ignores: ['dist/*'],
-  },
+export default defineConfig([
+	expoConfig,
+	{
+		ignores: ['dist/*', 'app-example/*'],
+	},
+	{
+		plugins: {
+			"@stylistic": stylistic,
+		},
+		rules: {
+			"camelcase": ["error", {
+				properties: "never",
+			}],
+			"no-console": ["error", {
+				allow: ["error"],
+			}],
+			"prefer-const": ["error"],
+			"func-names": ["error", "always"],
+			"curly": ["error", "all"],
+			"require-await": ["error"],
+			"no-else-return": ["error"],
+			"@stylistic/brace-style": ["error"],
+			"@stylistic/comma-dangle": ["error", "always-multiline"],
+			"@stylistic/object-curly-spacing": ["error", "always"],
+			"@stylistic/array-bracket-spacing": ["error", "never"],
+			"@stylistic/quote-props": ["error", "consistent-as-needed"],
+			"@stylistic/multiline-comment-style": ["error", "starred-block"],
+			"@stylistic/semi": ["error", "always"],
+		},
+	},
 ]);
